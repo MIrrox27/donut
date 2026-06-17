54feccf0ad13f6473ce9e347d361809d455f3659
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-17 17:58:12 +0500

    Update screen initialization to use spaces and add nested loops for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
+// author https://github.com/MIrrox27/donut
+// main.rs
+
+use std::io::{self, BufRead};
+
+
 fn main() {
     let width: usize = 120;
     let height: usize = 30;
 
-    let mut screen = vec!['@'; width * height + 1]; // Создаем массив с символами размещенными по всему экрану 
+    let screen = vec!['@'; width * height]; // Создаем массив с символами размещенными по всему экрану 
    
 
-    
-
-    for i in 0..width{
-        for j in 0..height{
-            let idx = i + j * width;
-            screen[idx] = '@';
-
-        }
-    }
     let output: String = screen.iter().collect(); 
 
     println!("{}", output);
+
+
     /*  Нужно для отрисовки пончика 
     let r1: i32; // Радиус маленькой окружности (внетренность)
     let r2: i32; // Радиус большей окружности 
@@ -27,8 +26,8 @@ fn main() {
     let x = (r2 + r1 * i.cos()) * j.cos();
     let y = r1 * i.sin();
     let z = (r2 + r1 * i.cos()) * j.sin();
-
-
-
     */
+
+    let stdin = io::stdin(); // чтобы сразу не закрывалась 
+    let _ = stdin.lock().read_line(&mut String::new());
 }
