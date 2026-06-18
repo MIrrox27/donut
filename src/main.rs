21f91a2e11d3e5f7ee0f1b2e7f675a170006731e
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-18 16:39:32 +0500

    Refactor screen initialization to use dynamic pixel assignment for rendering

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,30 @@ fn main() {
     let width: usize = 120;
     let height: usize = 30;
 
-    let screen = vec!['@'; width * height]; // Создаем массив с символами размещенными по всему экрану 
-   
+    let mut screen = vec![' '; width * height]; // Создаем массив с символами размещенными по всему экрану 
+    let mut nums: Vec<usize> = vec![0; width * height];
+    let aspect = height as f32 / height as f32;
+
+    for i in 0..width{
+        for j in 0..height{
+            
+
+            let x: f32 = (i as f32 / width as f32 * 2.0 - 1.0) * aspect;
+            let y: f32 = j as f32 / height as f32 * 2.0 - 1.0;
+
+            let mut pixel = ' ';
+            let index = i + j * width;
+
+
+            if (x*x + y*y) < 0.5 {pixel = '@';}  
+            screen[index] = pixel;
+            nums[index] = index;
+
+            
+        }
+
+    }
+
 
     let output: String = screen.iter().collect(); 
 
