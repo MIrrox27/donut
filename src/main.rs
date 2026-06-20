22a7ca9a1f22158422f0c4d5a8a6cb45a88a7cda
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-20 20:09:22 +0500

    Создан пончик

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,8 @@ fn main() {
 
 
         // Углы вращения вокруг осей X и Z
-    let a = 0_f32; 
-    let b = 0_f32; 
+    let mut a = 0_f32; 
+    let mut b = 0_f32; 
 
         // Радиусы
     let r1 = 1_f32; // Радиус маленькой окружности 
@@ -31,9 +31,10 @@ fn main() {
         // Углы закругления 
     let mut i: f32 = 0_f32;
     let mut j: f32 = 0_f32;
+    if i == 0.0 || j == 0.0 {} // Заглушка
 
     let step = 0.07_f32; // шаг для перебора i и j
-    let offset = 5_f32;
+    let offset = 4.5_f32;
 
 
 
@@ -55,10 +56,10 @@ fn main() {
 
                     // координаты точек
                 let x = (r2 + r1 * ic) * jc;
-                let y = r1 * i.sin();
+                let y = r1 * is;
                 let z = (r2 + r1 * ic) * js;
 
-                    // Вычисляем нормаль
+                    // Вычисляем нормаль до поворота
                 let nx = jc * ic; 
                 let ny = is;
                 let nz = ic * js;
@@ -68,42 +69,46 @@ fn main() {
 
                     // Поворачиваем нормаль 
                 let nx2 =rotate_normal.0;
-                let ny2 =rotate_normal.1;
-                let nz2 =rotate_normal.2;
+                //let ny2 =rotate_normal.1;
+                //let nz2 =rotate_normal.2;
 
+                    // Яркость 
+                let brightness = clamp(nx2, 0.0, 1.0);
+                let gradient = [' ', '.', ',', '-', '~', ':', ';', ':', '=', '!', '*', '#', '$', '@']; // @%#*+=-:. 
+                let gradient_size = gradient.len();
+                let idx = (brightness * (gradient_size - 1) as f32).round() as usize;
+                let pixel = gradient[idx];
+                   
+                    // Поворачиваем точки
                 let x2 = rotate_tor.0;
                 let y2 = rotate_tor.1;
                 let z2 = rotate_tor.2;
 
-                let d = 1_f32 / (z2 + offset);
+                let d = 1.0 / (z2 + offset);
 
-                let sx = clamp(center_x + scale_x * d * x2, 0_f32, width as f32) as usize;
-                let sy = clamp(center_y - scale_y * d * y2, 0_f32, height as f32) as usize;
+                let sx = clamp(center_x + scale_x * d * x2, 0_f32, (width-1) as f32) as usize;
+                let sy = clamp(center_y - scale_y * d * y2, 0_f32, (height-1) as f32) as usize;
 
-                let pixel = ' ';
+                
                 let index = sy * width + sx;
-
                 if d > z_buffer[index]{
                     z_buffer[index] = d;
-                    screen[index] = pixel;
+                    screen[index] = pixel; // Записываем символ
                 }
                 
-               
-                
-
-                
-
                 j += step;
             }
+            
             i += step;
+            j = 0.0;
         }
 
-
-
-    
         
         let output: String = screen.iter().collect(); 
         println!("{}", output);
+        a += 0.04; b += 0.02;
+        std::thread::sleep(std::time::Duration::from_millis(30));
+        
     }
 }
 
@@ -121,44 +126,6 @@ fn rotate(a:f32, b:f32, x:f32, y:f32, z:f32) -> (f32, f32, f32){
 }
 
 
-fn x(i: usize, width: usize, height: usize) -> f32{
-    let aspect = width as f32 / height as f32 * 11.0 / 24.0; // соотношение сторон, для исправления неровностей. 11/24 - соотношение сторон 1 символа. для окна 120х30 примерно равно 1.833
-    let x: f32 = (i as f32 / width as f32 * 2.0 - 1.0) * aspect;
-    return x;
-}
-
-
-fn y(j: usize, height: usize) -> f32{
-    let y: f32 = j as f32 / height as f32 * 2.0 - 1.0;
-    return y;
-
-}
-
-
-
 fn clamp(value: f32, min: f32, max: f32) -> f32{ // переписал функцию
     return min.max(value.min(max));
-}
-
-
-
-fn get_color(x: f32, y: f32) -> i32{
-    let dist = (x*x + y*y).sqrt();
-    let color = (1.0 / dist) as i32;
-    return color;
-
-}
-
-fn get_char(x: f32, y: f32) -> char{
-    let gradient = [' ', '.', ',', '-', '~', ':', ';', ':', '=', '!', '*', '#', '$', '@']; // @%#*+=-:. 
-    let gradient_size = gradient.len() - 2;
-
-    let mut pixel: char = ' ';
-    if pixel == ' ' {} // Заглушка
-    let mut color = get_color(x, y) as f32;
-
-    color = clamp(color, 0_f32, gradient_size as f32);
-    pixel = gradient[color as usize];
-    return pixel
-
 }
